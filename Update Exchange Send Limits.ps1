54616b670a42d1f9ea1b4a631665c7c9459f1c0c
Austin Downing <HanSolo552@gmail.com>
2018-10-29 10:28:15 -0400

    Add files via upload

---
+++ Update Exchange Send Limits.ps1
@@ -0,0 +1,8 @@
+﻿#Get users from text document
+#$users = get-content “Script Location"
+
+#Get Users from OU
+$users = Get-ADUser -Filter * -SearchBase "OU=Users,DC=Contoso,DC=COM" | Select UserPrincipalName
+foreach ($user in $users){
+Set-Mailbox -Identity $username -UseDatabaseQuotaDefaults $true -MaxReceiveSize "Unlimited" -MaxSendSize "Unlimited"
+}
